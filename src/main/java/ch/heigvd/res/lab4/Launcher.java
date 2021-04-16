cd9029bdc163739f6959e9f28114343a5c578a89
David González León <david.gonzalezleon@heig-vd.ch>
2021-04-16 15:22:31 +0200

    setup main function

--- src/main/java/ch/heigvd/res/lab4/Launcher.java
+++ src/main/java/ch/heigvd/res/lab4/Launcher.java
@@ -5,7 +5,20 @@
  */
 package ch.heigvd.res.lab4;
 
+import ch.heigvd.res.lab4.config.ConfigurationManager;
+import ch.heigvd.res.lab4.prank.PrankGenerator;
+import ch.heigvd.res.lab4.prank.mail.Mail;
+
+import java.util.ArrayList;
+
 public class Launcher {
    public static void main(String[] args) {
+      ConfigurationManager configurationManager = new ConfigurationManager();
+      SMTPClient client = new SMTPClient(configurationManager.getSmtpServerAddress(), configurationManager.getSmtpServerPort());
+      PrankGenerator generator = new PrankGenerator(configurationManager);
+      ArrayList<Mail> mails = generator.generatePranks();
+      for (Mail m : mails){
+         client.sendMail(m);
+      }
    }
 }
